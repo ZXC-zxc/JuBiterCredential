d1b453c30bd8e9a0dbbade46cc0ef0ce41b882ea
changliang0931 <changliang0931@126.com>
2020-06-11 18:22:15 +0800

    取数据

--- src/components/copyright/ownership.vue
+++ src/components/copyright/ownership.vue
@@ -1,9 +1,38 @@
 <!-- 所有权 -->
 <template>
-	<h1>1111111</h1>
+	<h1>111111{{claims.txs}}</h1>
 </template>
 
 <script>
+    import axios from "axios"
+	export default {
+		data: function() {
+			return {
+				claims:[],
+				count: 0,
+				page_number: 1,
+				page_total: 0,
+				limit: 10
+			}
+		},
+		async mounted() {
+			await this.fetchClaims()
+		},
+		methods: {
+			async fetchClaims() {
+				var json = await axios.get("http://192.168.17.84:8080/gocosmos/claim/queryOwnerShipTransactions?account=ftsafe1j7kzx9c86she9ne228jp0f8pc0jgsyzkqejftf")
+				this.claims = json.data.txs
+				this.count= json.data.count
+				this.page_number = json.data.page_number
+				this.page_total = json.data.page_total
+			},
+		},
+		watch: {
+			$route(to, from) {
+				fetchClaims()
+		   }
+		},
+	}
 </script>
 
 <style>
