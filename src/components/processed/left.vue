6d9e8c0e4d6ee1fadb3528cb2d239122338f08b9
JustinJIAch <13028695963@163.com>
2020-06-12 14:50:17 +0800

    测试登录

--- src/components/processed/left.vue
+++ src/components/processed/left.vue
@@ -1,85 +1,134 @@
 <template>
-	<section>
-		<div class="num">
-			<span>消息</span>
-			<span>5</span>
-		</div>
-		<ul>
-			<li class="active">XXX请求授权</li>
-			<li>XXX请求授权</li>
-			<li>XXX请求授权</li>
-			<li>XXX请求授权</li>
-			<li>XXX请求授权</li>
-		</ul>
-	</section>
+  <section>
+    <div class="num">
+      <span>消息</span>
+      <span>{{ total }}</span>
+    </div>
+    <ul>
+      <li class="active">{{ applys.list }}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
-	export default {
-		data: function() {
-			return {
-				
-			}
-		},
-		mounted() {
-			
-		},
-		components:{
-			
-		},
-		methods:{
-			
-		}
-	
-	}
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      applys: [],
+      total: 0
+    };
+  },
+  mounted() {
+    this.fetchApplys();
+  },
+  components: {},
+  beforeCreate: function() {},
+  methods: {
+    fetchApplys() {
+      axios
+        .post(
+          "jubiter-credential-web/admin/shiro/device/login.action",
+          {
+            data: { deviceSn: "JUB111" }
+          },
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*", //解决cors头问题
+              "Access-Control-Allow-Credentials": "true", //解决session问题
+              "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8" //将表单数据传递转化为request payload类型
+            },
+            withCredentials: true
+          }
+        )
+        .then(function(response) {
+          var res = response.data;
+          if (res.code == "ok-000000") {
+            axios
+              .post(
+                "jubiter-credential-web/admin/apply/list.action",
+                {
+                  pageSize: 10,
+                  pageNumber: 1,
+                  type: "initiate"
+                },
+                {
+                  headers: {
+                    "Access-Control-Allow-Origin": "*", //解决cors头问题
+                    "Access-Control-Allow-Credentials": "true", //解决session问题
+                    "Content-Type": "application/json; charset=UTF-8" //将表单数据传递转化为request payload类型
+                  },
+                  withCredentials: true
+                }
+              )
+              .then(function(response) {
+                var res = response.data;
+                alert(res);
+              })
+              .catch(function(error) {
+                alert(error);
+              });
+          }
+        })
+        .catch(function(error) {
+          alert(error);
+        });
+    }
+  },
+  watch: {
+    $route(to, from) {
+      fetchApplys();
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped="scoped">
-	section{
-		width: 37%;
-		background: #FFFFFF;
-		min-width: 170px;
-		max-width: 380px;
-		border-right: 1px solid #EFF1F3;
-		ul{
-			    margin: 0;
-			    list-style: none;
-			    /* margin: 20px 0 0 0; */
-			    width: 100%;
-			    padding: 50px 0 0 0;
-				li{
-					border-bottom: 1px solid #EFF1F3;
-					padding: 19px 5% 19px 11%;
-					font-size: 18px;
-					// background: #F6F7FA;
-				}
-				.active{
-				    position: relative;
-				}
-				.active::before{
-				  content: " ";
-				  border: 3px solid red;/*设置红色*/
-				  border-radius:3px;/*设置圆角*/
-				  position: absolute;
-				  // z-index: 1000;
-				  margin-top: 9px;
-				  left: 6%;
-				}
-		}
-		.num{
-			margin: 55px 20px 0 10%;
-			span{
-				margin: 0 20px 0 0;
-				font-size: 20px;
-			}
-			span:nth-child(2){
-				    padding: 1px 16px;
-				    background: #ECEEF0;
-				    color: #5C6771;
-				    font-size: 16px;
-				    border-radius: 16px;
-			}
-		}
-	}
+section {
+  width: 37%;
+  background: #ffffff;
+  min-width: 170px;
+  max-width: 380px;
+  border-right: 1px solid #eff1f3;
+  ul {
+    margin: 0;
+    list-style: none;
+    /* margin: 20px 0 0 0; */
+    width: 100%;
+    padding: 50px 0 0 0;
+    li {
+      border-bottom: 1px solid #eff1f3;
+      padding: 19px 5% 19px 11%;
+      font-size: 18px;
+      // background: #F6F7FA;
+    }
+    .active {
+      position: relative;
+    }
+    .active::before {
+      content: " ";
+      border: 3px solid red; /*设置红色*/
+      border-radius: 3px; /*设置圆角*/
+      position: absolute;
+      // z-index: 1000;
+      margin-top: 9px;
+      left: 6%;
+    }
+  }
+  .num {
+    margin: 55px 20px 0 10%;
+    span {
+      margin: 0 20px 0 0;
+      font-size: 20px;
+    }
+    span:nth-child(2) {
+      padding: 1px 16px;
+      background: #eceef0;
+      color: #5c6771;
+      font-size: 16px;
+      border-radius: 16px;
+    }
+  }
+}
 </style>
 
