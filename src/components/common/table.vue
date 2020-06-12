6c58d6187743d8f4a389f478a55ad061ceb5ede6
gaojiaojiao <gaojiaojiao@ftsafe.com>
2020-06-12 15:59:42 +0800

    著作权页面新增

--- src/components/common/table.vue
+++ src/components/common/table.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
-		<el-table :data="list" style="width: 100%" height="100%">
+		<el-table :data="list" style="width: 100%" height="100%" @row-click="getDetail">
 			<div  v-for="(item,index) in tabListData">
-				<el-table-column :prop="item.el_data" :label="item.title" ></el-table-column>
+				<el-table-column :prop="item.el_data" :label="item.title"></el-table-column>
 			</div>
 			
 				<!-- <el-table-column fixed prop="date" label="日期" ></el-table-column> -->
@@ -19,20 +19,40 @@
 
 <script>
 	import page from "@/components/common/page.vue"
-  export default {
-	  //tableData:接口返回的数据       tabListData：表格的列名（父组件定义）
-	  props:[
-		  "tableData","tabListData"
-	  ],
-	  data(){
-		  return{
-			  total:this.tableData.total,
-			  list:this.tableData.list
-		  }
-	  },
-	  components:{
-		  page
-	  },
-    
-  }
+	export default {
+		//tableData:接口返回的数据       tabListData：表格的列名（父组件定义）
+		props:[
+			"tableData","tabListData"
+		],
+		data(){
+			return{
+				total:this.tableData.total,
+				list:this.tableData.list
+			}
+		},
+		components:{
+			page
+		},
+		mounted() {
+			// if()
+		},
+		methods:{
+			//存证证书详情页
+			getDetail(row) {
+				alert(row.id)
+				this.$parent.detailShow(row.id);
+				// this.rowId = row.id
+				// this.$emit("rowId",row.id);
+			},
+			
+			// childMethod(id) {
+			// 	this.$parent.detailShow(id);
+			// }
+		},
+		// watch:{
+		// 	rowId:function(val){
+		// 		console.log(val)
+		// 	}
+		// }
+	}
 </script>
