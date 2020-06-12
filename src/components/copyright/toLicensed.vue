d134de28a0f391fd865cf082463aa979d4883f16
gaojiaojiao <gaojiaojiao@ftsafe.com>
2020-06-12 16:04:49 +0800

    著作权页面新增

---
+++ src/components/copyright/toLicensed.vue
@@ -0,0 +1,35 @@
+<!-- 授权给 -->
+<template>
+	<inputAndBtn :tempData="tempData"></inputAndBtn>
+</template>
+
+<script>
+	import inputAndBtn from "@/components/common/inputAndBtn.vue"
+	export default {
+		data: function() {
+			return {
+				// id:this.$route.params.id,
+				tempData:{
+					path:"",   //跳转路径 
+					title:"授权给",
+					btn:"授权",
+					
+					// readonly:""     //只读：readonly   可输入：" "
+				}
+			}
+		},
+		created() {
+
+		},
+		components:{
+			inputAndBtn
+		},
+		
+		watch: {
+		},
+	}
+</script>
+
+<style lang="scss" scoped="scoped">
+</style>
+
