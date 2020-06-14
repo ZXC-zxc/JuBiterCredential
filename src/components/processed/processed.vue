7c7d3a3909706116aac65733315fb7da30b1704b
changliang0931 <changliang0931@126.com>
2020-06-14 15:34:36 +0800

    我的待处理

--- src/components/processed/processed.vue
+++ src/components/processed/processed.vue
@@ -1,53 +1,74 @@
 <template>
-	<div class="process">
-		<left></left>
-		<right></right>
-		<!-- <section>
-			<router-view name="statement"></router-view>
-		</section>	 -->
-	</div>
+  <div class="process">
+    <left v-if="flag"></left>
+    <right v-if="flag" :selectedApply.sync="selectedApply"></right>
+  </div>
 </template>
 
 <script>
-	import left from "@/components/processed/left.vue"
-	import right from "@/components/processed/right.vue"
-	
-	export default {
-		data: function() {
-			return {
-				
-			}
-		},
-		components:{
-			left,right
-		},
-	}
+import left from "@/components/processed/left.vue";
+import right from "@/components/processed/right.vue";
+
+export default {
+  data: function() {
+    return {
+      flag: false,
+      applys: [],
+      total: 0,
+      selectedApply: { hash: "", to: "" }
+    };
+  },
+  components: {
+    left,
+    right
+  },
+  beforeCreate() {
+    var self = this;
+    self.$store
+      .dispatch("getTableData", {
+        pageNumber: 1,
+        pageSize: 10,
+        tableType: 4
+      })
+      .then(() => {
+        self.applys = self.$store.state.list;
+        self.total = self.$store.state.total;
+        self.flag = true;
+      });
+  },
+  methods: {
+    //子组件调
+    showSelectedApply(selectedApply) {
+      this.selectedApply.hash = selectedApply.hash;
+      this.selectedApply.to = selectedApply.initiaterAddr;
+    }
+  }
+};
 </script>
 
 <style  lang="scss" scoped="scoped">
-	a{
-		text-decoration: none;
-		color: #181818;
-	}
-	.process{
-		width: 100%;
-		height: 100%;
-		box-sizing: border-box;
-		display: flex;
-		section{
-			    
-		}
-		ul{
-			margin: 0;
-		}
-		section:nth-child(2){
-			flex: 1;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			flex-direction: column;
-		}
-	}
+a {
+  text-decoration: none;
+  color: #181818;
+}
+.process {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  display: flex;
+  section {
+  }
+  ul {
+    margin: 0;
+  }
+  section:nth-child(2) {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+}
 </style>
 
 
--- src/components/processed/right.vue
+++ src/components/processed/right.vue
@@ -1,61 +1,63 @@
 <template>
-	<section>
-		<div>
-			<label>请求您授权</label>
-			<span></span>
-		</div>
-		<div>
-			<label>给</label>
-			<span></span>
-		</div>
-	</section>
+  <section>
+    <div>
+      <label>请求您授权</label>
+      <span>{{apply.hash}}</span>
+    </div>
+    <div>
+      <label>给</label>
+      <span>{{apply.to}}</span>
+    </div>
+  </section>
 </template>
 
 <script>
-	export default {
-		data: function() {
-			return {
-				
-			}
-		},
-		mounted() {
-			
-		},
-		components:{
-			
-		},
-		methods:{
-			
-		}
-	
-	}
+export default {
+  props: ["selectedApply"],
+  data() {
+    return {
+      apply: this.selectedApply || { hash: "", to: "" }
+    };
+  },
+  watch: {
+    selectedApply: {
+      handler: function(newVal, oldVal) {
+        if (newVal) {
+          this.apply = newVal;
+        }
+      },
+      immediate: true,
+      deep: true
+    }
+  },
+  methods: {}
+};
 </script>
 
 <style lang="scss" scoped="scoped">
-	section{
-		flex: 1;
-		div{
-			width: 70%;
-			max-width: 470px;
-			label{
-				display: block;
-				margin-bottom: 20px;
-				font-size: 20px;
-			}
-			span{
-				display: block;
-				min-width: 200px;
-				max-width: 600px;
-				/* border: 1px solid; */
-				height: 50px;
-				background-color: white;
-				box-shadow: 0px 0px 9px rgba(0,0,0,.2);
-				border-radius: 10px;
-			}
-			
-		}
-		div:nth-child(1){
-			margin-bottom: 30px;
-		}
-	}
+section {
+  flex: 1;
+  div {
+    width: 70%;
+    max-width: 470px;
+    label {
+      display: block;
+      margin-bottom: 20px;
+      font-size: 20px;
+    }
+    span {
+      display: block;
+      min-width: 200px;
+      max-width: 600px;
+      /* border: 1px solid; */
+      height: 50px;
+      background-color: white;
+      box-shadow: 0px 0px 9px rgba(0, 0, 0, 0.2);
+      border-radius: 10px;
+    }
+  }
+  div:nth-child(1) {
+    margin-bottom: 30px;
+  }
+}
 </style>
